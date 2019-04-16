91db1f5bfcbb914f1bb12d31e09094aba3f5e0bb
revdancatt <github@revdancatt.com>
2019-04-16 17:03:43 +0100

    Allow updating of users

--- src/modules/schema/admin.js
+++ src/modules/schema/admin.js
@@ -2,18 +2,41 @@ const base = require('./base.js')
 
 exports.schema = `
 
+type Roles {
+  isAdmin: Boolean
+  isDev: Boolean
+}
+
 type Instance {
   id: String!
   title: String
 }
 
+type User {
+  apitoken: String
+  id: String
+  lastLoggedIn: String
+  lastUpdated: String
+  roles: Roles
+  instances: [Instance]
+}
+
 type Query {
   ${base.query.hello}
-  instances: [Instance]
+  instances(
+    ids: [String]
+  ): [Instance]
   instance(
     id: String!
   ): Instance
+  users(
+    ids: [String]
+  ): [User]
+  user(
+    id: String!
+  ): User
 }
+
 
 type Mutation {
   createInstance(
@@ -26,6 +49,12 @@ type Mutation {
   deleteInstance(
     id: String!
   ): Instance
+  updateUser(
+    id: String!
+    instances: [String]
+    isAdmin: Boolean
+    isDev: Boolean
+  ): User
 }
 
 `
